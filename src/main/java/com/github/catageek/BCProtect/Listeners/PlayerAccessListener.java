6a346199ac739f8cfdf8e67aa63c31310d1182d7
catageek <catageek@free.fr>
2013-04-30 09:29:02 +0200

    Add the possibility to disable player vehicle exit Fix a bug with player access that prevented player to travel by cart

--- src/main/java/com/github/catageek/BCProtect/Listeners/PlayerAccessListener.java
+++ src/main/java/com/github/catageek/BCProtect/Listeners/PlayerAccessListener.java
@@ -1,18 +1,23 @@
 package com.github.catageek.BCProtect.Listeners;
 
 import org.bukkit.Location;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.vehicle.VehicleExitEvent;
 
+import com.github.catageek.BCProtect.BCProtect;
 import com.github.catageek.BCProtect.Util;
 
 public class PlayerAccessListener implements Listener {
 
 	@EventHandler (ignoreCancelled = true)
 	public void onPlayerMove(PlayerMoveEvent event) {
-		if (! Util.checkPermission(event.getPlayer(), event.getTo(), "canAccess")) {
+		Player player = event.getPlayer();
+		if (! player.isInsideVehicle() && ! Util.checkPermission(player, event.getTo(), "canAccess")) {
 			event.setCancelled(true);
 			Location loc = event.getFrom();
 			event.setFrom(loc.subtract(loc.getDirection()));
@@ -21,8 +26,19 @@ public class PlayerAccessListener implements Listener {
 
 	@EventHandler (ignoreCancelled = true)
 	public void onPlayerTeleport(PlayerTeleportEvent event) {
-		if (! Util.checkPermission(event.getPlayer(), event.getTo(), "canAccess")) {
+		Player player = event.getPlayer();
+		if (! player.isInsideVehicle() && ! Util.checkPermission(player, event.getTo(), "canAccess")) {
 			event.setCancelled(true);
 		}
 	}
+	
+	@EventHandler (ignoreCancelled = true)
+	public void onVehicleExit(VehicleExitEvent event) {
+		LivingEntity entity = event.getExited();
+		if (entity instanceof Player)
+		if (! Util.checkPermission((Player) entity, event.getVehicle().getLocation(BCProtect.location), "canAccess")) {
+			event.setCancelled(true);
+		}
+	}
+
 }
